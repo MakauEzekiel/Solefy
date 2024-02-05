6c1401c22a138b9c405dbd30bc6a3b31e2fa8b04
EzekielMakau <ezemakau@gmail.com>
2024-02-05 05:59:39 +0200

    commit

---
+++ src/app/sales/[category]/page.tsx
@@ -0,0 +1,67 @@
+'use client';
+import React, { useEffect, useState } from "react";
+import ProductList from "@/components/ProductList";
+import Footer from "@/components/footer";
+import { db } from '../../firebaseConfig';
+import { collection, getDocs} from 'firebase/firestore';
+import Image from "next/image";
+import bgMen from '@/asserts/menBgOne.jpg'
+import bgWomen from '@/asserts/womenBgOne.jpg'
+
+async function fetchSalesFromFirestore(collectionName: string) {
+    const querySnapshot = await getDocs(collection(db, collectionName));
+    const data: any = [];
+    querySnapshot.forEach((doc) => {
+      data.push({id:doc.id, ...doc.data()});
+    });
+    return data;
+  }
+
+export default function Products ({params}:any) {
+    const category = params.category;
+    const [womenSalesData, setwomenSalesData] = useState([]);
+    const [menSalesData, setmenSalesData] = useState([]);
+    const [data, setData] = useState([]);
+
+    useEffect(() => {
+        async function fetchData() {
+          const womenData = await fetchSalesFromFirestore('women_sales');
+          const menData = await fetchSalesFromFirestore('men_sales');
+          
+          setwomenSalesData(womenData);
+          setmenSalesData(menData);
+
+          if(category === 'women') {
+            setData(womenData);
+          }
+          else if(category === 'men') {
+            setData(menData);
+          }
+          else {
+            setData(womenData.concat(menData));
+          }
+        }
+        fetchData();
+      }, [category])
+
+    useEffect(() => {
+    },);
+    return (
+        <div className="w-full">
+          <div className="w-full">
+            <Image
+                src={category === 'men' ? bgMen : bgWomen}
+                alt="Description of the image"
+                style={{objectFit:"cover"}}
+                unoptimized
+                className='relative w-full h-[400px] z-[0]'
+            />
+            <div className="w-full sticky fixed absolute mt-[-200px] pl-16">
+                <h2 className="uppercase text-white text-6xl font-bold">{category} SALES</h2>
+            </div>
+          </div>
+            <ProductList data={data}/>
+            <Footer/>
+        </div>
+    )
+}
---
+++ src/components/ProductList.tsx
@@ -0,0 +1,67 @@
+import React, { useState } from 'react'
+import Image from 'next/image'
+import Link from 'next/link'
+
+const ProductList = ({data}:any) => {
+    // const [currentSaleCategory, setCurrentSaleCategory] = useState(true);
+    // const [loading, setLoading] = useState(true);
+    let currentSaleCategory = 0;
+    function setCurrentSaleCategory(flag: number) {
+        currentSaleCategory = flag;
+    }
+
+
+
+    // if (loading) {
+    //     return <main className="z-[999999999999999999] w-[100%] absolute  text-center item-center justify-center top-1/2 left-1/2 transform -translate-x-1/2 -translate-y-1/2 p-4">
+    //         <div className="w-full h-full">
+    //             <div className="flex justify-center items-center">
+    //                 <div className="animate-spin rounded-full h-32 w-32 border-t-2 border-b-2 border-black"></div>
+    //             </div>
+    //         </div>
+    //     </main>;
+    //   }
+    
+  return (
+    <main className='w-full h-full mt-[107px] relative'>
+        {/* <div className='h-[70vh] grid hidden md:flex items-center md:grid-cols-2 grid-cols-1 w-full mx-auto md:pl-16 md:pr-16 pt-16 sm:pl-8 sm:pr-8'>
+            
+        </div> */}
+        <div className='w-full sm:p-16 sm:pt-24 pl-4 pr-4 pt-8'>
+            <div className='grid gap-4 lg:grid-cols-4 md:grid-cols-2 sm:grid-cols-1 '>
+            {data.map((sale:any) => (
+                <React.Fragment key={sale.product_id}>
+                    {(currentSaleCategory === 0) && (
+                        <>
+                        {setCurrentSaleCategory(3)}
+                        <div className='col-span-2 text-black min-w-[100px] overflow-hidden'>
+                            <Link href={`/product-details/${sale.product_id}`}>
+                                <Image src={sale.image} alt="" unoptimized priority placeholder='blur' blurDataURL={sale.base64} loading="eager" width={100} height={100} className='w-full md:max-h-[448px] object-cover hover:scale-105 transform transition-transform ease-in-out duration-300 cursor-pointer'/>
+                            </Link>
+                        </div>
+                        </>
+                    )}
+                    <>
+                    {setCurrentSaleCategory(currentSaleCategory-1)}
+                    <Link  href={`/product-details/${sale.product_id}`}>
+                        <div className='col-span-1 text-black min-w-[100px] overflow-hidden'>
+                            <Image src={sale.image} alt="" unoptimized priority placeholder='blur' blurDataURL={sale.base64} loading="eager" width={100} height={100} className='w-full md:max-h-[400px] object-cover hover:scale-105 transform transition-transform ease-in-out duration-300 cursor-pointer'/>
+                            <div className='max-h-[84px]'>
+                                <div className='font-bold'>
+                                    <p className='uppercase text-[16px] mt-[16px]'>{sale.name}</p>
+                                </div>
+                                <p className='text-gray-400 text-base text-[14px]'>{sale.category}</p>
+                                <p className='font-bold text-xl mt-2'>{sale.price}</p>
+                            </div>
+                        </div>
+                    </Link>
+                    </>
+                </React.Fragment>
+            ))}
+            </div>
+        </div>
+    </main>
+  )
+}
+
+export default ProductList
